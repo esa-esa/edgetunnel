c78caeb5245444a903173269b9c3d461871d1f4d
esa-esa <59135640+esa-esa@users.noreply.github.com>
2025-04-13 22:26:06 +0330

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'f85356d9-1af8-40b8-a788-1910b3ca2d7a';
 
 let proxyIP = '';
 
